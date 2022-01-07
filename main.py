166653360391bef3bb30ace0c8e89cdd115bc818
jlacasse <johny.lacasse2@videotron.ca>
2022-01-06 20:44:10 -0500

    .

---
+++ main.py
@@ -0,0 +1,17 @@
+#https://gist.github.com/ghostbitmeta/694934062c0814680d52
+
+sys_mode = "econo"  #cool/heat/econo
+
+eq_fan = True
+eq_emergency_heat = False
+eq_compressor_stage_1 = False
+eq_ob_valve = False
+
+th_1_setpoint = 21
+th_1_temperature = 21
+th_1_fan_status = False
+th_1_ob_valve = False
+th_1_hysteresis = 0.5
+
+snd_ext_temp = 4
+snd_ext_hum = 50
