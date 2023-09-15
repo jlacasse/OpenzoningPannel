2eabc52c2e2fec40c05fd1d2f8b9dfcf68660d1b
jlacasse <johny.lacasse2@videotron.ca>
2023-09-14 22:29:44 -0400

    .

--- testgpt.py
+++ testgpt.py
@@ -2,15 +2,32 @@
 # 2018-05-05
 # Version 1.0
 
+# import smbus
+# import time
+
+# bus = smbus.SMBus(1)
+# address = 0x21
+
+# def read_all():
+#     for i in range(0, 22):
+#         print("Register: ", i, " Value: ", bus.read_byte_data(address, i))
+#         time.sleep(0.1)
+        
+# read_all()
+
+# create function to read all pin (as input ) on mcp2317 at address 0x22
+# 2018-05-05
+# Version 1.0
+
 import smbus
 import time
 
 bus = smbus.SMBus(1)
-address = 0x21
+address = 0x22
 
 def read_all():
     for i in range(0, 22):
-        print("Register: ", i, " Value: ", bus.read_byte_data(address, i))
+        print("Pin: ", i, " Value: ", bus.read_byte_data(address, i))
         time.sleep(0.1)
         
 read_all()
