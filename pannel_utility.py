5cfb32fbb7886f6d85d0cfd2fb069d623b64d269
Johny Lacasse <johny.lacasse2@videotron.ca>
2023-09-20 04:00:09 -0400

    backup 2023-09-20

--- pannel_utility.py
+++ pannel_utility.py
@@ -30,30 +30,48 @@ def read_mcp_input(mcp_address):
             pinsMcp_string_array.append({keys[i]: "true"})
     return pinsMcp_string_array
 
-pinsMcp1 = read_mcp_input(0x20)
-pinsMcp2 = read_mcp_input(0x21)
-pinsMcp3 = read_mcp_input(0x22)
-pinsMcp4 = read_mcp_input(0x23)
-#split les input de mcpx (16 in) en thermostat (8in)
-zones = {"th1": pinsMcp1[:8]} , {"th2": pinsMcp1[8:]} , {"th3": pinsMcp2[:8]} , {"th4": pinsMcp2[8:]}, {"th5": pinsMcp3[:8]} , {"th6": pinsMcp3[8:]}, {"th7": pinsMcp4[:8]} , {"th8": pinsMcp4[8:]}
+def read_all_mcp_out_json():
+    pinsMcp1 = read_mcp_input(0x20)
+    pinsMcp2 = read_mcp_input(0x21)
+    pinsMcp3 = read_mcp_input(0x22)
+    pinsMcp4 = read_mcp_input(0x23)
+    #split les input de mcpx (16 in) en thermostat (8in)
+    zones = {"th1": pinsMcp1[:8]} , {"th2": pinsMcp1[8:]} , {"th3": pinsMcp2[:8]} , {"th4": pinsMcp2[8:]}, {"th5": pinsMcp3[:8]} , {"th6": pinsMcp3[8:]}, {"th7": pinsMcp4[:8]} , {"th8": pinsMcp4[8:]}
 
-# Convertis le JSON original en Python dictionary
-original_dict = json.loads(json.dumps(zones))
+    # Convertis le JSON original en Python dictionary
+    original_dict = json.loads(json.dumps(zones))
 
-# Création d'un dictionary vide pour stocker le JSON formaté
-formatted_dict = {"zones": {}}
+    # Création d'un dictionary vide pour stocker le JSON formaté
+    formatted_dict = {"zones": {}}
 
-# Loop dans le dictionnaire original et format le JSON
-for item in original_dict:
-    for key, value in item.items():
-        zones = {}
-        for val in value:
-            for k, v in val.items():
-                zones[k] = v
-        formatted_dict["zones"][key] = zones
+    # Loop dans le dictionnaire original et format le JSON
+    for item in original_dict:
+        for key, value in item.items():
+            zones = {}
+            for val in value:
+                for k, v in val.items():
+                    zones[k] = v
+            formatted_dict["zones"][key] = zones
 
-# Converti le dictionary formaté en string JSON
-formatted_json = json.dumps(formatted_dict)
+    # Converti le dictionary formaté en string JSON
+    formatted_json = json.dumps(formatted_dict)
 
-#output (payload)
-print(formatted_json)
+    return (formatted_json)
+
+#if one of the thermostat fan is on, the fan status is on
+def define_system_fan_status():
+    all_th_json = json.loads(read_all_mcp_out_json()) 
+    for th in all_th_json["zones"]:
+        if all_th_json["zones"][th]["g"] == "true":
+            return True
+        else:
+            return False
+        
+def define_th_mode(th):
+    all_th_json = json.loads(read_all_mcp_out_json()) 
+    if all_th_json["zones"][th]["y1"] == "false":
+        return "off"
+    elif all_th_json["zones"][th]["ob"] == "true":
+        return "heat"
+    else:
+        return "cool"
